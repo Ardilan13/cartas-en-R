56560864e817f04d91bbf0f36d31829ebf275efe
juanFeRoz <juanfeliperojas2@protonmail.com>
2024-07-03 16:21:02 -0500

    Quite setwd, ahora toca cambiarlo desde RStudio

--- blackjack.R
+++ blackjack.R
@@ -263,7 +263,6 @@ play_turn <- function(money, deck, count) {
 # Inicio del juego
 money <- enter_money()
 if (!is.null(money)) {
-  setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #Hace que el working directory sea la ubicacion del archivo
   deck <- read.csv("./cards.csv")
   deck <- build_deck(deck)
   count <- 0 # Inicializar el conteo de cartas
